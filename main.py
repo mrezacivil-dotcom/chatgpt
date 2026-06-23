5618f13a6bb73c803432c04f3e6bdc9d3d15b5d2
mrezacivil-dotcom <mrezacivil@gmail.com>
2026-06-24 03:10:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 import requests
-import time
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 def send_signal(signal):
+
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 text = (
     "🚀 TRADE SIGNAL\n\n"
     f"Symbol: {signal['symbol']}\n"
     f"Direction: {signal['direction']}\n"
-    f"Score: {signal['score']}\n\n"
+    f"Score: {signal['score']}\n"
     f"Entry: {signal['entry']}\n"
     f"SL: {signal['sl']}\n"
     f"TP: {signal['tp']}"
@@ -28,27 +28,13 @@ response = requests.post(
 
 print(response.text)
 
-def run():
+signal = {
+"symbol": "BTCUSDT",
+"direction": "BUY",
+"score": 80,
+"entry": 65000,
+"sl": 64000,
+"tp": 67000
+}
 
-symbols = [
-    "BTCUSDT",
-    "ETHUSDT",
-    "SOLUSDT"
-]
-
-for symbol in symbols:
-
-    signal = {
-        "symbol": symbol,
-        "direction": "BUY",
-        "score": 80,
-        "entry": 100,
-        "sl": 95,
-        "tp": 110
-    }
-
-    send_signal(signal)
-    time.sleep(1)
-
-if name == "main":
-run()
+send_signal(signal)
