a3f6b3f45b01507ac5f937e2f4644210ff6dd45d
mrezacivil-dotcom <mrezacivil@gmail.com>
2026-06-24 03:13:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 def send_signal(signal):
-
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 text = (
@@ -13,12 +12,9 @@ text = (
     f"Symbol: {signal['symbol']}\n"
     f"Direction: {signal['direction']}\n"
     f"Score: {signal['score']}\n"
-    f"Entry: {signal['entry']}\n"
-    f"SL: {signal['sl']}\n"
-    f"TP: {signal['tp']}"
 )
 
-response = requests.post(
+requests.post(
     url,
     data={
         "chat_id": CHAT_ID,
@@ -26,15 +22,10 @@ response = requests.post(
     }
 )
 
-print(response.text)
-
 signal = {
 "symbol": "BTCUSDT",
 "direction": "BUY",
-"score": 80,
-"entry": 65000,
-"sl": 64000,
-"tp": 67000
+"score": 80
 }
 
 send_signal(signal)
