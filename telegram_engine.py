1c85b69073b0607d3cdf2c003f6e4348a5a3e89b
mrezacivil-dotcom <mrezacivil@gmail.com>
2026-06-24 19:53:40 +0330

    Update telegram_engine.py

--- telegram_engine.py
+++ telegram_engine.py
@@ -1,32 +1,26 @@
 import requests
+import os
 
-BOT_TOKEN = "YOUR_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-URL = f"https://api.telegram.org/bot8819076931:AAGgdHQWlxlZ1f_zQ3LIXIcjhW8_Z0nz8ks/sendMessage"
-
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 def send_signal(signal):
-
     text = f"""
-🚀 V62 SIGNAL
+🚀 SIGNAL
 
 {signal['symbol']} {signal['direction']}
 
-Entry: {signal['entry']:.4f}
-SL: {signal['sl']:.4f}
-TP: {signal['tp']:.4f}
+Entry: {signal['entry']}
+SL: {signal['sl']}
+TP: {signal['tp']}
 
 Score: {signal['score']}
-Conf: {signal['confidence']}%
+Confidence: {signal['confidence']}
 """
 
-    try:
-        requests.post(URL, data={
-            "chat_id": 5039122077,
-            "text": text
-        })
-        print("📨 TG SENT OK")
-
-    except Exception as e:
-        print("TG ERROR:", e)
+    requests.post(URL, data={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
