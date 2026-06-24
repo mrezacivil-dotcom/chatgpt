d04688750098b6f4992b6a863540b582059d9d93
mrezacivil-dotcom <mrezacivil@gmail.com>
2026-06-24 19:53:21 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
+import os
+
 LIVE_TRADING = False
+MAX_POSITIONS = int(os.getenv("MAX_POSITIONS", "3"))
+SLEEP_TIME = int(os.getenv("SLEEP_TIME", "5"))
 
-MAX_POSITIONS = 3
-SLEEP_TIME = 5
-
-MIN_SCORE = 2.0
-CACHE_SECONDS = 5
+MIN_SCORE = float(os.getenv("MIN_SCORE", "2.0"))
+CACHE_SECONDS = int(os.getenv("CACHE_SECONDS", "5"))
 
 ENABLE_FUNDING = True
 ENABLE_NEWS_FILTER = False
-
-import os
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
